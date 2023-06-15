1835a9234591f6e5827d51ddbfb140c6a755b66a
dellematti <116433753+dellematti@users.noreply.github.com>
2023-06-15 09:27:31 +0200

    Delete Bancarelle directory

--- Bancarelle/Listino.java
+++
@@ -1,7 +0,0 @@
-interface Listino{
-    // REQUIRES : ---
-    // MODIFIES : ---
-    // EFFECTS : restituisce il prezzo dell acquisto di una determinata quantità di un tipo di giocattolo
-    //           se quantità <= 0 viene lanciata un eccezione di tipo IllegalArgumentException
-    public int prezzo(final int quantità, final Giocattolo giocattolo);
-}
